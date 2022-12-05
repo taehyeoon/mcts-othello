a1d4c9130415a7d81836a7018b6c6f72fbb46aee
lkmisem <lkmisem@konkuk.ac.kr>
2022-12-05 19:55:04 +0900

    오차 계산 합수

---
+++ OthelloNN/utils.py
@@ -0,0 +1,10 @@
+import torch
+class AlphaLoss(torch.nn.Module):
+    def __init__(self):
+        super(AlphaLoss, self).__init__()
+
+    def forward(self, y_value, value, y_policy, policy):
+        value_error = (value - y_value) ** 2
+        policy_error = torch.sum((-policy* (1e-8 + y_policy.float()).float().log()), 1)
+        total_error = (value_error.view(-1).float() + policy_error).mean()
+        return total_error
